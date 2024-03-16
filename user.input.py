21f185479d0ebdb876608b8b0dee01d0af99b659
shiku-kimani <161102735+shiku-kimani@users.noreply.github.com>
2024-03-16 06:51:02 +0300

    Add files via upload

---
+++ user.input.py
@@ -0,0 +1,5 @@
+#user_input.py
+name= input("what is your name?")
+age= input("how old are you?")
+     location= input("where do you live?")
+print(hello(name),your (age) years old and live in (location).)
